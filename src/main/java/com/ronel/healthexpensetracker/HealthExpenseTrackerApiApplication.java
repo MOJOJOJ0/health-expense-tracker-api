a2b5114dcf0a71d47933a7a8abbec70637b1d181
Ronel Peter <ronel.sajan@pm.me>
2022-02-03 20:06:41 -0600

    initialized spring

---
+++ src/main/java/com/ronel/healthexpensetracker/HealthExpenseTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.ronel.healthexpensetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthExpenseTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthExpenseTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ronel/healthexpensetracker/HealthExpenseTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronel.healthexpensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthExpenseTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
